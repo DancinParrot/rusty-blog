20799144b4547e6a55440337b047351b4c9ca560
Kai <kaizhetan@yahoo.com>
2022-10-05 01:49:23 +0800

    Commented out non-functional parts - Note datetime conversion imcompatibility

--- src/components/note/note_index.rs
+++ src/components/note/note_index.rs
@@ -11,22 +11,22 @@ use chrono::{DateTime, Utc};
 use wasm_bindgen_futures;
 use reqwasm::http::*;
 
-#[serde_with::serde_as]
-#[derive(Properties, PartialEq, Serialize, Deserialize)]
-pub struct Task {
-    pub note_id: String,
-    pub content: String,
-    #[serde_as(as = "TimestampMilliSeconds<String, Flexible>")]
-    pub date_created: DateTime<Utc>,
-    #[serde_as(as = "TimestampMilliSeconds<String, Flexible>")]
-    pub date_updated: DateTime<Utc>,
-    pub title: String,
-    pub notebook_id: String 
-}
+// #[serde_with::serde_as]
+// #[derive(Properties, PartialEq, Serialize, Deserialize)]
+// pub struct Task {
+//     pub note_id: String,
+//     pub content: String,
+//     #[serde_as(as = "serde_with::DurationSeconds<i64>")]
+//     pub date_created: DateTime<Utc>,
+//     #[serde_as(as = "TimestampMilliSeconds<String, Flexible>")]
+//     pub date_updated: DateTime<Utc>,
+//     pub title: String,
+//     pub notebook_id: String 
+// }
 
-pub struct TaskResponse {
-    pub data: Vec<Task>,
-}
+// pub struct TaskResponse {
+    // pub data: Vec<Task>,
+// }
 
 #[function_component(NoteIndex)]
 pub fn note_index() -> Html {
@@ -34,25 +34,25 @@ pub fn note_index() -> Html {
         log!("form content is", data.content);
     });
 
-    let onload = {
-        Callback::from(move |_| {
-            wasm_bindgen_futures::spawn_local(async move {
-                    let response = Request::get("http://localhost:8080/get-all-notes")
-                    .send()
-                    .await
-                    .unwrap()
-                    .json::<TaskResponse>()
-                    .await
-                    .unwrap();
-                    log!(serde_json::to_string_pretty(&response).unwrap());
-            })
-        })
-    };
+    // let onload = {
+    //     Callback::from(move |_| {
+    //         wasm_bindgen_futures::spawn_local(async move {
+    //                 let response = Request::get("http://localhost:8080/get-all-notes")
+    //                 .send()
+    //                 .await
+    //                 .unwrap()
+    //                 .json::<TaskResponse>()
+    //                 .await
+    //                 .unwrap();
+    //                 log!(serde_json::to_string_pretty(&response).unwrap());
+    //         })
+    //     })
+    // };
 
     html! {
         <div class="">
             // List all notes
-            <Navbar />
+            //<Navbar />
             <NoteForm onsubmit={form_submit} />
         </div>
     }
